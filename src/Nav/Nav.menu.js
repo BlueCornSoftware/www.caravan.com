a79fa0b4ae2c56bda234d0e65630ec77f9796041
JakeTheCorn <jcorn@fusionalliance.com>
2018-05-15 21:53:53 -0400

    adding material design

--- src/Nav/Nav.menu.js
+++ src/Nav/Nav.menu.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 class NavMenu extends React.Component {
   render() {
     return (
       <div>
-        <button onClick={() => {
-          window.location.href = 'www.google.com'
+        <Button onClick={() => {
+          console.log('HEY')
         }}>
           NAV ONE
-        </button>
+        </Button>
       </div>
     );
   }
