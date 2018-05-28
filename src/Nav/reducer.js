9fc5503fe729d7404851e70cf2208619c839bb58
Kalalau <cantrellkalalau@gmail.com>
2018-05-27 21:18:28 -0400

    removed filename prefixes that redundantly reference the component name

--- src/Nav/reducer.js
+++ src/Nav/reducer.js
@@ -1,15 +1,14 @@
-import * as types from './Nav.types';
-import InitialState from './Nav.initial-state';
+import * as types from "./types";
+import InitialState from "./initial-state";
 
 export default function NavReducer(state = InitialState, action) {
   switch (action.type) {
     case types.HELLO:
       return {
         ...state,
-        hello: true,
+        hello: true
       };
     default:
       return state;
   }
 }
-
