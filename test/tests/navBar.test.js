3efd13cc7746fa6f70f7c625d7421039f72c6b12
JakeTheCorn <jcorn@fusionalliance.com>
2018-06-03 15:20:54 -0400

    CAR-9 fixing await to use headless

--- test/tests/navBar.test.js
+++ test/tests/navBar.test.js
@@ -1,12 +1,12 @@
 const puppeteer = require('puppeteer');
 
-describe('Booleans', () => {
-  it('Should be true', async (done) => {
-    const browser = await puppeteer.launch({
-      headless: false,
-    });
+
+describe('The Nav Bar', () => {
+  it('should have an initial title of "Caravan"', async (done) => {
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('http://localhost:3000');
+    await page.waitForSelector('span.PageTitle');
     const menuTitle = await page.$eval('span.PageTitle', el => el.innerText);
     await browser.close();
     expect(menuTitle).toBe('Caravan');
